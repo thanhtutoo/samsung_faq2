d2029488d057997598610bef540626c11ecb0b4d
thanhtutoo <5531305090@lamduan.mfu.ac.th>
2016-11-22 17:31:49 +0630

    second

--- index.js
+++ index.js
@@ -1,53 +1,32 @@
 'use strict'
 
-var express = require('express')
-var bodyParser = require('body-parser')
-var request = require('request')
+const express = require('express')
+const bodyParser = require('body-parser')
+const request = require('request')
+const app = express()
 
-var Config = require('./config')
-var FB = require('./connectors/facebook')
-var Bot = require('./bot')
+app.set('port', (process.env.PORT || 5000))
 
+// Process application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({extended: false}))
 
-// LETS MAKE A SERVER!
-var app = express()
-app.set('port', (process.env.PORT) || 5000)
-// SPIN UP SERVER
-app.listen(app.get('port'), function () {
-  console.log('Running on port', app.get('port'))
-})
-// PARSE THE BODY
+// Process application/json
 app.use(bodyParser.json())
 
-
-// index page
+// Index route
 app.get('/', function (req, res) {
-  res.send('hello world i am a chat bot')
+    res.send('Hello world, I am a chat bot')
 })
 
-// for facebook to verify
-app.get('/webhooks', function (req, res) {
-  if (req.query['hub.verify_token'] === Config.FB_VERIFY_TOKEN) {
-    res.send(req.query['hub.challenge'])
-  }
-  res.send('Error, wrong token')
-})
-
-// to send messages to facebook
-app.post('/webhooks', function (req, res) {
-  var entry = FB.getMessageEntry(req.body)
-  // IS THE ENTRY A VALID MESSAGE?
-  if (entry && entry.message) {
-    if (entry.message.attachments) {
-      // NOT SMART ENOUGH FOR ATTACHMENTS YET
-      FB.newMessage(entry.sender.id, "That's interesting!")
-    } else {
-      // SEND TO BOT FOR PROCESSING
-      Bot.read(entry.sender.id, entry.message.text, function (sender, reply) {
-        FB.newMessage(sender, reply)
-      })
+// for Facebook verification
+app.get('/webhook/', function (req, res) {
+    if (req.query['hub.verify_token'] === 'my_voice_is_my_password_verify_me') {
+        res.send(req.query['hub.challenge'])
     }
-  }
+    res.send('Error, wrong token')
+})
 
-  res.sendStatus(200)
+// Spin up the server
+app.listen(app.get('port'), function() {
+    console.log('running on port', app.get('port'))
 })
